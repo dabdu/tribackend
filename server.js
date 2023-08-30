e45aa2af66f5d0f759bab2ef5e0429e48dbd7e3e
Ibrahim <ibrahim.dauda@koboweb.com>
2023-08-30 04:20:22 +0100

    Push

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const port = process.env.PORT;
 
 connectDB();
 const app = express();
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://tribackoffice.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 // Middlewares
 // app.use(
 //   cors({
@@ -16,14 +21,10 @@ const app = express();
 //     credentials: true,
 //   })
 // );
+// res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Add this line
+
 // Middleware to set up CORS headers
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://tribackoffice.vercel.app");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Add this line
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
